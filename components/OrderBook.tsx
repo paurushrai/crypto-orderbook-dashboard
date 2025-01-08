94bd251b65cedfe86a7b3e8e11450542346b72f4
Paurush Rai <paurushrai99@gmail.com>
2025-01-08 17:48:09 +0530

    error boundary implementation

--- components/OrderBook.tsx
+++ components/OrderBook.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import { Card } from '@/components/ui/card';
-import SpreadIndicator from './SpreadIndicator';
-import OrderbookImbalance from './OrderbookImbalance';
-import MarketDepth from './MarketDepth';
-import OrderbookTable from './OrderbookTable';
-import { useMarketData } from '@/hooks/use-market-data';
+import { Card } from "@/components/ui/card";
+import SpreadIndicator from "./SpreadIndicator";
+import OrderbookImbalance from "./OrderbookImbalance";
+import MarketDepth from "./MarketDepth";
+import OrderbookTable from "./OrderbookTable";
+import { useMarketData } from "@/hooks/use-market-data";
+import ErrorBoundaryUsingClass from "@/components/ErrorBoundary";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "@/components/ErrorFallback";
+import BugCounter from "./BugCounter";
 
 export type OrderBookEntry = {
   price: number;
@@ -20,14 +24,29 @@ export type OrderBookData = {
 
 export default function OrderBook() {
   const orderBookData = useMarketData();
-  const spread = orderBookData.asks[0]?.price - orderBookData.bids[0]?.price || 0;
+  const spread =
+    orderBookData.asks[0]?.price - orderBookData.bids[0]?.price || 0;
 
   return (
     <div className="grid gap-6">
       <Card className="p-6">
+        <h1>Error Boundary Using Class Component</h1>
+        <ErrorBoundaryUsingClass>
+          <BugCounter />
+        </ErrorBoundaryUsingClass>
+        <h1>
+          Error Boundary Using Function Component: react-error-boundary library
+        </h1>
+        <p>Recommended modern approach</p>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <BugCounter />
+        </ErrorBoundary>
         <h2 className="mb-4 text-2xl font-semibold">Market Indicators</h2>
         <div className="space-y-6">
-          <SpreadIndicator spread={spread} timestamp={orderBookData.timestamp} />
+          <SpreadIndicator
+            spread={spread}
+            timestamp={orderBookData.timestamp}
+          />
           <OrderbookImbalance
             bids={orderBookData.bids}
             asks={orderBookData.asks}
